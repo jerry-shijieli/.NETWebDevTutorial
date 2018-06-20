257f24c96219b43f3bcbe3570c1b3c147a6032c1
Jerry <sli41@ncsu.edu>
2018-06-20 11:19:51 -0700

    Exercise 2 Display Customers and Movies
    List customers and movies using IEnumerable data strucutre. Also add link to each Customer redirecting to its detailed profile using @HTML.ActionLink() on view page

--- WebApplication/WebApplication/Controllers/MoviesController.cs
+++ WebApplication/WebApplication/Controllers/MoviesController.cs
@@ -12,6 +12,23 @@ namespace WebApplication.Controllers
 {
     public class MoviesController : Controller
     {
+        [Route("Movies/index")]
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name="Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -42,13 +59,13 @@ namespace WebApplication.Controllers
             return Content("id = "+id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (sortBy.IsNullOrWhiteSpace())
-                sortBy = "Name";
-            return Content(String.Format("PageIndex={0}&SortBy={1}", pageIndex, sortBy));
-        }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (sortBy.IsNullOrWhiteSpace())
+        //        sortBy = "Name";
+        //    return Content(String.Format("PageIndex={0}&SortBy={1}", pageIndex, sortBy));
+        //}
     }
 }
