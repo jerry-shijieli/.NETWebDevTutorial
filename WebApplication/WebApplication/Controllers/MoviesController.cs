a47bcac1452367a8f4618a0905e30db5d972ecf3
Jerry <sli41@ncsu.edu>
2018-07-30 00:17:51 -0700

    Exercise 4 - Add Movie form
    Add Movie Form to create or edit movie profile; use @HTML.ActionLink to link Movie item to its Edit action; use model binding to pass the Movie and Genres list to form view; Save button will call Save action in Movies controller to save created\updated data to DB using _context.SaveChanges() and RedirectToAction() of index page.

--- WebApplication/WebApplication/Controllers/MoviesController.cs
+++ WebApplication/WebApplication/Controllers/MoviesController.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Microsoft.Ajax.Utilities;
 using WebApplication.Models;
 using WebApplication.ViewModels;
 
@@ -23,6 +21,40 @@ namespace WebApplication.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres =  genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleasedDate = movie.ReleasedDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         [Route("Movies/index")]
@@ -79,7 +111,18 @@ namespace WebApplication.Controllers
 
         public ActionResult Edit(int id)
         {
-            return Content("id = "+id);
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
 
         //public ActionResult Index(int? pageIndex, string sortBy)
--- WebApplication/WebApplication/Models/Movie.cs
+++ WebApplication/WebApplication/Models/Movie.cs
@@ -9,14 +9,22 @@ namespace WebApplication.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleasedDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
