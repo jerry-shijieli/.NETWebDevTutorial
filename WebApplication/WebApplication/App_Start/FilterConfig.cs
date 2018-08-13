e3c47fa4161b5e41cd9406fefbf1d77a0a6ae329
Jerry <sli41@ncsu.edu>
2018-08-13 01:43:21 -0700

    Enable facebook(FB) login
    Added RequireHttpsAttribute filter to enable SSL login only; Added DL attribute to AccountController, ExternalLoginConfirmation and related view; Added FB developer app info to Startup.Auth to get access permission.

--- WebApplication/WebApplication/App_Start/FilterConfig.cs
+++ WebApplication/WebApplication/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace WebApplication
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
--- WebApplication/WebApplication/App_Start/Startup.Auth.cs
+++ WebApplication/WebApplication/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace WebApplication
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "",
+               appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
