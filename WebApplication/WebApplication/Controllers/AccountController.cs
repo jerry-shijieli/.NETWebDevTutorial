2410a435478a9c5e0a52c2373a35ce055c1ae1a0
Jerry <sli41@ncsu.edu>
2018-08-13 21:31:51 -0700

    Exercise 8 - Extract ExternalLoginConfirmationViewModel and RegisterViewModel to separate class files
    Extract classes into separate files to reduce the cohension and improve maintainance.

--- WebApplication/WebApplication/Controllers/AccountController.cs
+++ WebApplication/WebApplication/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using WebApplication.Models;
+using WebApplication.ViewModels;
 
 namespace WebApplication.Controllers
 {
--- WebApplication/WebApplication/Models/AccountViewModels.cs
+++ WebApplication/WebApplication/Models/AccountViewModels.cs
@@ -3,20 +3,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    //public class ExternalLoginConfirmationViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DrivingLicense { get; set; }
+    //    [Required]
+    //    [Display(Name = "Driving License")]
+    //    public string DrivingLicense { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Phone { get; set; }
-    }
+    //    [Required]
+    //    [StringLength(50)]
+    //    public string Phone { get; set; }
+    //}
 
     public class ExternalLoginListViewModel
     {
@@ -70,32 +70,32 @@ namespace WebApplication.Models
         public bool RememberMe { get; set; }
     }
 
-    public class RegisterViewModel
-    {
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DrivingLicense { get; set; }
+    //public class RegisterViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Driving License")]
+    //    public string DrivingLicense { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Phone { get; set; }
+    //    [Required]
+    //    [StringLength(50)]
+    //    public string Phone { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Password")]
+    //    public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Confirm password")]
+    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    //    public string ConfirmPassword { get; set; }
+    //}
 
     public class ResetPasswordViewModel
     {
